dbd49621612bb39e8dd5ed1fb7dc6ceb1496fbdc
manishkhrn1 <manishkhrn1@gmail.com>
2023-06-10 09:37:54 -0400

    added a password variable and added a loop

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,11 @@
 from random import randint
 
-lowercase = randint(97,122)
-uppercase = randint(65,90)
-numbers = randint(0,9)
+password = ""
+
+for i in range(12):
+    randomSelector = randint(0,2)
+    lowercase = randint(97,122)
+    uppercase = randint(65,90)
+    numbers = randint(48,57)
+
+    
