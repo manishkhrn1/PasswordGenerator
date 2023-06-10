9b23efd0a26d0c578ecca33bd9b82869f2e48138
manishkhrn1 <manishkhrn1@gmail.com>
2023-06-10 10:14:09 -0400

    Added a function to select a normalPassword

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,18 +1,22 @@
 from random import randint
 
-password = ""
 
-for i in range(12):
-    randomSelector = randint(0,2)
-    lowercase = randint(97,122)
-    uppercase = randint(65,90)
-    numbers = randint(48,57)
 
-    if randomSelector == 0 :
-        password += chr(lowercase)
-    elif randomSelector == 1:
-        password += chr(uppercase)
-    elif randomSelector == 2:
-        password += chr(numbers)
-print(password)
+def NormalPassword():
+    password = ""
+    for i in range():
+        randomSelector = randint(0,3)
+        lowercase = randint(97,122)
+        uppercase = randint(65,90)
+        numbers = randint(48,57)
+
+        if randomSelector == 0 or randomSelector == 1 :
+            password += chr(lowercase)
+        elif randomSelector == 2:
+            password += chr(uppercase)
+        elif randomSelector == 3:
+            password += chr(numbers)
+    print(password)
     
+
+    NormalPassword()
