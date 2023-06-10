c695d2b86fbc55809003643fee5a0cbcf5839cf3
manishkhrn1 <manishkhrn1@gmail.com>
2023-06-10 09:42:13 -0400

    added conditions to fullfil the requirement

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -8,4 +8,11 @@ for i in range(12):
     uppercase = randint(65,90)
     numbers = randint(48,57)
 
+    if randomSelector == 0 :
+        password += chr(lowercase)
+    elif randomSelector == 1:
+        password += chr(uppercase)
+    elif randomSelector == 2:
+        password += chr(numbers)
+print(password)
     
